30dae640fd66589b3a36bcb3625f327b3d8ac74d
vien2024 <thieuminhvien@gmail.com>
2024-03-06 23:19:16 +0700

    Change footer

--- E-co-main/src/Components/Footer/Footer.jsx
+++ E-co-main/src/Components/Footer/Footer.jsx
@@ -4,35 +4,37 @@ import footer_logo from '../Assets/logo_big.png'
 import instagram_icon from '../Assets/instagram_icon.png'
 import pinsterster_icon from '../Assets/pintester_icon.png'
 import whatsapp_icon from '../Assets/whatsapp_icon.png'
+import logo from '../Assets/logo.png'
 
 const Footer = () => {
   return (
-    <div className='footer'>
-       <div className="footer">
-        <img src={footer_logo} alt="" />
-        <p>SHOPPER</p>
-       </div>
-       <ul className="footer-links">
-        <li>Company</li>
-        <li>Product</li>
-        <li>Offices</li>
-        <li>About</li>
-        <li>Contact</li>
-       </ul>
-       <div className="footer-social-icon">
-        
-          <img src={instagram_icon} alt="" />
-        
-          <img src={pinsterster_icon} alt="" />
-        
-          <img src={whatsapp_icon} alt="" />
-        
-       </div>
-       <div className="footer-copyright">
-        <hr />
-        <p>Copyright @ 2023 - All Right Reserved.</p>
-       </div>
-    </div>
+      <div className='footer-container'>
+        <div className='footer'>
+          <div className="nav-logo">
+            <img src={logo} alt="" />
+          </div>
+          <div className="footer-heading footer-1">
+            <h2>Watch</h2>
+            <a href="#">Pricing</a>
+          </div>
+          <div className="footer-heading footer-2">
+            <h2>Resources</h2>
+            <a href="#">Blog</a>
+            <a href="#">User guides</a>
+            <a href="#">Webinars</a>
+          </div>
+          <div className="footer-heading footer-3">
+            <h2>Company</h2>
+            <a href="#">About</a>
+            <a href="#">Join us</a>
+          </div>
+          <div className='footer-email-form'>
+            <h2 className='join'>Join our newsletter</h2>
+            <input types="email" placeholder='Enter your email address' id="footer-email"></input>
+            <input type="submit" value="Sign up" id="footer-email-btn"></input>
+          </div>
+        </div>
+      </div>
   )
 }
 
