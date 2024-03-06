7c7bce23e12ef7609672aae579d1ecee422c6877
vien2024 <thieuminhvien@gmail.com>
2024-03-06 21:03:24 +0700

    Change navigation bar

--- E-co-main/src/Components/Navbar/Navbar.jsx
+++ E-co-main/src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
+import user_icon from '../Assets/avatar.png'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
@@ -10,19 +11,17 @@ const Navbar = () => {
   
     return (
     <div className='navbar'>
-      <div className="nav-log">
-        <img src={logo} alt="" />
-        <p>Shopping ddd</p>
-      </div>
       <ul className="nav-menu">
-        <li onClick={()=>{setMenu("shop")}}><Link style={{ textDecoration: 'none'}} to='/'>Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("mens")}}><Link style={{ textDecoration: 'none'}} to='/mens'>Men</Link>{menu==="mens"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("womens")}}><Link style={{ textDecoration: 'none'}} to='/womens'>Women</Link>{menu==="womens"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("kids")}}><Link style={{ textDecoration: 'none'}} to='/kids'>Kids</Link>{menu==="kids"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("watches")}}><Link style={{ textDecoration: 'none'}} to='/'>Watches</Link>{menu==="watches"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("about")}}><Link style={{ textDecoration: 'none'}} to='/mens'>About us</Link>{menu==="about"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("contact")}}><Link style={{ textDecoration: 'none'}} to='/womens'>Contact</Link>{menu==="contact"?<hr/>:<></>}</li>
       </ul>
+      <div className="nav-logo">
+        <img src={logo} alt="" />
+      </div>
       <div className="nav-login-cart">
-        <Link to='/login'><button>Login</button></Link>
-        <Link to='/cart'><img src={cart_icon} alt=""/></Link>
+        <Link to='/login'><img src={user_icon} alt="" className='user_icon'/></Link>
+        <Link to='/cart'><img src={cart_icon} alt="" className='cart_icon'/></Link>
         <div className="nav-cart-count">0</div>
       </div>
     </div>
