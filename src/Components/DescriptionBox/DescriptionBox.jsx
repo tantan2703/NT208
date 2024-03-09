b8cafdb815a07e5929ac1031f8c8a808d5a7093e
chily2265 <hazakachi@gmail.com>
2024-03-09 16:10:38 +0700

    productdetail

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './DescriptionBox.css'
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>dsnvklsndlknnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
+            nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
+            nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn</p>
+        <p>
+            adnsvkjldsnavkjlnsadkljvnaskldvnkljadsvjklsdnavkjnsdkvjaskl
+            vsnakdvnjkadsvkjdnsvkjlnsadkvjlksdkjladsnvkjlsadnkvjlnsadkvjl
+            asdvklnjkldsvjklsnadvkjndsvkjladsnvlkjnsdakvjlnsakldnvkj</p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import './CSS/LoginSignup.css'
 const LoginSignup = () => {
   return (
-    <div>
+    <div className='loginsignup'>
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder='Your Name' />
+          <input type="email" placeholder='Email Address' />
+          <input type="password" placeholder='Password' />
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">Already have an account?</p>
+        <div className="loginsign-agree">
+          <input type="checkbox" name="" id="" />
+          <p>By continue, i agree to the terms of use & privacy policy.</p>
+        </div>
+      </div>
       
     </div>
   )
