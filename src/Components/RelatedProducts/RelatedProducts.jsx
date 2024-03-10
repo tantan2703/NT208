c84959b28e578a9275c8bbed726f762f20e4694c
chily2265 <hazakachi@gmail.com>
2024-03-10 14:10:02 +0700

    productdetail-total

--- src/Components/RelatedProducts/RelatedProducts.jsx
+++ src/Components/RelatedProducts/RelatedProducts.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import './RelatedProducts.css'
+import data_product from '../Assets/data'
+import Item from '../Item/Item'
 const RelatedProducts = () => {
   return (
-    <div>
-      
+    <div className='relatedproducts'>
+        <h1>Related Products</h1>
+        <br />
+        <div className="relatedproducts-item">
+            {data_product.map((item,i)=>{
+                    return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+            })}
+        </div>
     </div>
   )
 }
