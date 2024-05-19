69c52b7fd2a703015c7ef152466c5bf7a851718c
Đạt <datnguyen101004@gmail.com>
2024-05-19 22:51:49 +0700

    landing page

---
+++ frontend/src/Components/ContentSlider/ContentSlide.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ContentSlide.css";
+
+const ContentSlide = () => {
+  return (
+    <div className="content_slider">
+      <h3>New watches in 2024</h3>
+    </div>
+  );
+};
+
+export default ContentSlide;
